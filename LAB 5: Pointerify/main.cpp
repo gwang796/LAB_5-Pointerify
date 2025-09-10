47886c2a55bb9ca53ed7ad1824e1ce91fab8c95d
Guo An Wang <you@example.com>
2025-09-10 14:23:00 -0700

    Start commit

---
+++ LAB 5: Pointerify/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+// COMSC-210 | LAB 5: Pointerify | Guo An Wang
+// IDE used: Xcode
+//  Created by Guo An Wang on 9/10/25.
+//
+
+#include <iostream>
+using namespace std;
+
+int swap(int &, int &);
+
+int main() {
+    int x = 5, y = 10;
+    cout << "x = " << x << "  | y = " << y << endl;
+
+    cout << "Swapping...\n";
+    int sum = swap(x, y);
+    cout << "x = " << x << " | y = " << y << endl;
+    cout << "sum = " << sum << endl;
+    return 0;
+}
+
+int swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+    return a + b;
+}
